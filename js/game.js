df5722ee56cce006e72c7db1b49d15aaa081c044
Ярослав Нестеров <atomofiron@yandex.ru>
2017-04-09 12:16:50 +0300

    pinky log

--- js/game.js
+++ js/game.js
@@ -22,8 +22,11 @@ function startUpdateLoop() {
     window.setInterval(update, 16);
 }
 
-
 Leap.loop(controllerOptions, function(frame) {
 	if (frame.hands.length > 0)
 		parseHand(frame.hands[0])
 })
+
+function parseHand(hand) {
+	clog(hand.pinky.bones[0].direction())
+}
