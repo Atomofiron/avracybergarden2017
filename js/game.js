a143790a3bd80c3fa621f301eab45720386ff551
Rasie1 <rasielll@gmail.com>
2017-04-09 13:22:34 +0300

    Add some methods

--- js/game.js
+++ js/game.js
@@ -5,13 +5,44 @@ function get(id) {
 var clog = console.log
 
 function init() {
+  var boxEl = get('bone0_2')
+  boxEl.addEventListener('mousedown', function () {
+    clog("asd")
+    tempShootCaller()
+  });
+}
+
+function tempShootCaller() {
+    shoot({
+        x: 0,
+        y: 0,
+        z: 0
+    },
+    {
+        x: 0,
+        y: 90,
+        z: 0
+    })
+}
+
+function spawnEnemy(position, speed, scale) {
 
 }
 
+function shoot(position, direction) {
+    clog("aasas")
+}
 
 function update() {
 
-    // var mything = get("mything")
+    var a = get("bone0_1")
+
+    a.setAttribute('position', {
+        x: 3,
+        y: 3,
+        z: 3
+    })
+    // a = 
     // var position = mything.getAttribute('position');
 
     // mything.setAttribute('position', {
