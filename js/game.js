04bd38ec10245701b06e222dc6979e83f538e463
Rasie1 <rasielll@gmail.com>
2017-04-09 11:51:14 +0300

    Add game loop

--- js/game.js
+++ js/game.js
@@ -8,5 +8,13 @@ function init() {
 
 
 function update() {
+}
 
+window.onload = function start() {
+    init();
+    startUpdateLoop();
+}
+
+function startUpdateLoop() {
+    window.setInterval(update, 16);
 }
